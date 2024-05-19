a8f8e0fec3d242ef3a29ed8dc177129a43e8827a
milkfist <alyonazzz12@mail.ru>
2024-05-19 20:42:21 +0300

    Part3-LearningAuthorization

--- app/src/main/java/com/example/firebase_messenger/MainActivity.java
+++ app/src/main/java/com/example/firebase_messenger/MainActivity.java
@@ -1,14 +1,85 @@
 package com.example.firebase_messenger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MY_LOG: MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if (currentUser == null) {
+//            mAuth.createUserWithEmailAndPassword("alyonazzz12@mail.ru", "123456")
+//                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if (task.isSuccessful()) {
+//                                // Sign in success, update UI with the signed-in user's information
+//                                FirebaseUser user = mAuth.getCurrentUser();
+//                                Log.d(TAG, "createUserWithEmail" + user.getUid() + ":success");
+//                            } else {
+//                                // If sign in fails, display a message to the user.
+//                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//                                Toast.makeText(MainActivity.this, "Authentication failed.",
+//                                        Toast.LENGTH_SHORT).show();
+//                            }
+//                        }
+//                    });
+//        } else {
+//            Log.d(TAG, "Authorized currentUser " + currentUser.getUid());
+//            mAuth.signOut();
+//            mAuth.signInWithEmailAndPassword("email@gmail.com", "password");
+            mAuth.signInWithEmailAndPassword("alyonazzz12@mail.ru", "111111")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+//        }
+
+//        mAuth.sendPasswordResetEmail("alyonazzz12@mail.ru");
+
+        mAuth.addAuthStateListener(firebaseAuth -> {
+            if (mAuth.getCurrentUser() != null) {
+                Log.d(TAG, "CurrentUser " + mAuth.getCurrentUser().getUid());
+            } else {
+                Log.d(TAG, "Not authorized");
+            }
+        });
     }
 }
